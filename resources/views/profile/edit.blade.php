30445465e9c6b279d726c37ca72fb808b2404da3
Eugene702 <eugenefeilianputrarangga@gmail.com>
2025-05-20 06:58:37 +0700

    Feat: Menyesuaikan sidebar pada halaman profile untuk masing masing role

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,24 +6,35 @@
         </h2>
     </x-slot>
 
-    <div class="flex min-h-screen"> {{-- Tambah flex untuk layout horizontal --}}
-        {{-- Sidebar di kiri --}}
-        @include('components.sidebar-admin')
+    <div class="flex min-h-screen">
+        @php
+            $role = auth()->user()->roles()->first()->name;
+        @endphp
 
-        <div class="flex flex-1 justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div class="max-w-3xl w-full space-y-6">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-profile-information-form')
+        @if ($role == 'KepalaSekolah')
+            @include('components.sidebar-kepsek')
+        @elseif ($role == 'Admin')
+            @include('components.sidebar-admin')
+        @elseif ($role == 'Guru')
+            @include('components.sidebar-guru')
+        @else
+            @include('components.sidebar-siswa')
+        @endif
+
+            <div class="flex flex-1 justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-3xl w-full space-y-6">
+                    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                        <div class="max-w-xl">
+                            @include('profile.partials.update-profile-information-form')
+                        </div>
                     </div>
-                </div>
 
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-password-form')
+                    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                        <div class="max-w-xl">
+                            @include('profile.partials.update-password-form')
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     </div>
 </x-app-layout>
